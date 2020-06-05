a0c519fc2a7bdb1c06f6e0499b31d0499c1ce2b5
Muhammad Nouman Atiq <36809880+nomisatti@users.noreply.github.com>
2020-06-05 18:35:09 +0500

    Added Number to Words Code

--- src/App.js
+++ src/App.js
@@ -3,25 +3,81 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+var unitsMap =  [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ];
+var tensMap =  ['',"", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ];
+var number = Number(prompt("Enter any number"));
+number = NumberToWords(number);
+alert(number);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>ABC 123</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
     </div>
   );
 }
+
+function NumberToWords (number) {
+
+  /*
+      if (number == 0)
+      return "zero";
+  
+  if (number < 0)
+      return "minus " + NumberToWords(Math.Abs(number));
+      */
+  
+  var words = "";
+  
+  if (parseInt(number / 100000) > 0)
+  {
+      indexof =  parseInt(number / 100000) ;
+      words += unitsMap[indexof] + " lakh "
+      number %= 100000;
+  } 
+  
+  if (parseInt(number / 1000) > 0)
+  {    
+      if (words != "")
+      words += "and ";
+      indexof = parseInt((number / 1000))
+      if(indexof>20){
+          indexof = parseInt((indexof / 10))
+  
+  
+      }
+      words += unitsMap[indexof] + " thousand ";
+      number %= 1000;
+  
+   
+  }
+  if (parseInt(number / 100) > 0)
+  {
+      if (words != "")
+      words += "and ";
+  
+      indexof = parseInt((number / 100))
+       words += unitsMap[indexof] + " hundred ";
+       number %= 100;
+  }
+  
+  if (number > 0)
+  {
+      if (words != "")
+          words += "and ";
+  
+    
+      if (number < 20)
+          words += unitsMap[number];
+      else
+      {
+          indexof = parseInt(number / 10);
+          words += tensMap[indexof];
+          if ((number % 10) > 0)
+              words += "-" + unitsMap[number % 10];
+      }
+  }
+  
+  return words;
+  }
 
 export default App;
