433f574e0a48e1a1dafa2f14f70b60c6d86fb761
Muhammad Nouman Atiq <36809880+nomisatti@users.noreply.github.com>
2020-06-06 16:05:19 +0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
+ <div class="container">
+    <div class="form-group">
+        <label for="number">Number:</label>
+        <input type='text' id='number' class="form-control" placeholder='Enter any Number' onkeyup="NumberToWords()"/>
+    </div>
 
-
-
-var number = Number(prompt("Enter any number"));
-number = NumberToWords(number);
-alert(number);
+    <div class="form-group">
+        <label for="words">Words:</label>
+        <input type='text' id='words' class="form-control" />
+    </div>
+</div>  
   return (
     <div className="App">
        
@@ -16,72 +21,71 @@ alert(number);
   );
 }
 
-function NumberToWords (number) {
+function NumberToWords () {
 
-  var unitsMap =  [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ];
-var tensMap =  ['',"", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ];
+    var unitsMap =  [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ];
+    var tensMap =  ['',"", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ];
 
-  var indexof = 0;
-  /*
-      if (number == 0)
-      return "zero";
+    var indexof = 0;
+
+    var number = document.getElementById('number').value
+
+    var words = "";
+    if (number == 0)
+        words += "zero";
+
+    if (parseInt(number / 1000000) > 0)
+    {
+        indexof =  parseInt(number / 1000000) ;
+        words += unitsMap[indexof] + " million "
+        number %= 1000000;
+    } 
+
+    if (parseInt(number / 100000) > 0)
+    {
+        indexof =  parseInt(number / 100000) ;
+        words += unitsMap[indexof] + " lakh "
+        number %= 100000;
+    } 
+
+    if (parseInt(number / 1000) > 0)
+    {    
   
-  if (number < 0)
-      return "minus " + NumberToWords(Math.Abs(number));
-      */
+        indexof = parseInt((number / 1000))
+        if(indexof>20){
+            indexof = parseInt((indexof / 10))
+         }
+         words += unitsMap[indexof] + " thousand ";
+         number %= 1000;
+    }
+    if (parseInt(number / 100) > 0)
+    {
   
-  var words = "";
+        indexof = parseInt((number / 100))
+        words += unitsMap[indexof] + " hundred ";
+        number %= 100;
+    }
+
+    if (number > 0)
+    {  
+        if (words !== "")
+             words += "and ";
+
   
-  if (parseInt(number / 100000) > 0)
-  {
-      indexof =  parseInt(number / 100000) ;
-      words += unitsMap[indexof] + " lakh "
-      number %= 100000;
-  } 
-  
-  if (parseInt(number / 1000) > 0)
-  {    
-      if (words !== "")
-      words += "and ";
-      indexof = parseInt((number / 1000))
-      if(indexof>20){
-          indexof = parseInt((indexof / 10))
-  
-  
-      }
-      words += unitsMap[indexof] + " thousand ";
-      number %= 1000;
-  
-   
-  }
-  if (parseInt(number / 100) > 0)
-  {
-      if (words !== "")
-      words += "and ";
-  
-      indexof = parseInt((number / 100))
-       words += unitsMap[indexof] + " hundred ";
-       number %= 100;
-  }
-  
-  if (number > 0)
-  {
-      if (words !== "")
-          words += "and ";
-  
-    
-      if (number < 20)
-          words += unitsMap[number];
-      else
-      {
-          indexof = parseInt(number / 10);
-          words += tensMap[indexof];
-          if ((number % 10) > 0)
-              words += "-" + unitsMap[number % 10];
-      }
-  }
-  
-  return words;
-  }
+        if (number < 20)
+            words += unitsMap[number];
+        else
+        {
+            indexof = parseInt(number / 10);
+            words += tensMap[indexof];
+        if ((number % 10) > 0)
+            words += " " + unitsMap[number % 10];
+        }
+    }
+
+    document.getElementById('words').value = words
+
+}
+
 
 export default App;
